bf0724658336a2faeaca20c2b15b3de1c18c6c12
Alex Johnson <johnsonav1992@gmail.com>
2022-12-31 13:16:52 -0600

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,104 +1,104 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
-    "rules": {
-      "indent": [
-        "error",
-        4,
-        {
-          "SwitchCase": 1
-        }
-      ],
-      "max-len": [
-        "warn",
-        {
-          "code": 80,
-          "ignorePattern": "\\s",
-          "ignoreTemplateLiterals": true
-        }
-      ],
-      "comma-style": [
-        "warn",
-        "first",
-        {
-          "exceptions": {
-            "ObjectPattern": false,
-            "ArrayPattern": false
-          }
-        }
-      ],
-      "comma-spacing": [
-        "error",
-        {
-          "before": false,
-          "after": true
-        }
-      ],
-      "comma-dangle": [
-        "error",
-        "never"
-      ],
-      "quotes": [
-        "error",
-        "single",
-        {
-          "allowTemplateLiterals": true
-        }
-      ],
-      "linebreak-style": [
-        "warn",
-        "unix"
-      ],
-      "space-in-parens": [
-        "error",
-        "always"
-      ],
-      "array-bracket-spacing": [
-        "error",
-        "always"
-      ],
-      "computed-property-spacing": [
-        "error",
-        "always"
-      ],
-      "object-curly-spacing": [
-        "error",
-        "always"
-      ],
-      "template-curly-spacing": [
-        "error",
-        "always"
-      ],
-      "brace-style": [
-        "warn",
-        "1tbs",
-        {
-          "allowSingleLine": true
-        }
-      ],
-      "strict": [
-        "warn",
-        "global"
-      ],
-      "space-before-function-paren": [
-        "error",
-        "always"
-      ],
-      "no-unused-vars": [
-        "warn",
-        {
-          "args": "none"
-        }
-      ],
-      "semi": [
-        "error",
-        "always"
-      ],
-      "no-case-declarations": "off",
-      "no-unsafe-finally": "off",
-      "no-unreachable": "warn",
-      "no-undef": "warn",
-      "no-empty-pattern": "warn",
-  }
-}
+    extends: [ '@remix-run/eslint-config', '@remix-run/eslint-config/node' ]
+    , 'rules': {
+        'indent': [
+            'error'
+            , 4
+            , {
+                'SwitchCase': 1
+            }
+        ]
+        , 'max-len': [
+            'warn'
+            , {
+                'code': 80
+                , 'ignorePattern': '\\s'
+                , 'ignoreTemplateLiterals': true
+            }
+        ]
+        , 'comma-style': [
+            'warn'
+            , 'first'
+            , {
+                'exceptions': {
+                    'ObjectPattern': false
+                    , 'ArrayPattern': false
+                }
+            }
+        ]
+        , 'comma-spacing': [
+            'error'
+            , {
+                'before': false
+                , 'after': true
+            }
+        ]
+        , 'comma-dangle': [
+            'error'
+            , 'never'
+        ]
+        , 'quotes': [
+            'error'
+            , 'single'
+            , {
+                'allowTemplateLiterals': true
+            }
+        ]
+        , 'linebreak-style': [
+            'warn'
+            , 'unix'
+        ]
+        , 'space-in-parens': [
+            'error'
+            , 'always'
+        ]
+        , 'array-bracket-spacing': [
+            'error'
+            , 'always'
+        ]
+        , 'computed-property-spacing': [
+            'error'
+            , 'always'
+        ]
+        , 'object-curly-spacing': [
+            'error'
+            , 'always'
+        ]
+        , 'template-curly-spacing': [
+            'error'
+            , 'always'
+        ]
+        , 'brace-style': [
+            'warn'
+            , '1tbs'
+            , {
+                'allowSingleLine': true
+            }
+        ]
+        , 'strict': [
+            'warn'
+            , 'global'
+        ]
+        , 'space-before-function-paren': [
+            'error'
+            , 'always'
+        ]
+        , 'no-unused-vars': [
+            'warn'
+            , {
+                'args': 'none'
+            }
+        ]
+        , 'semi': [
+            'error'
+            , 'always'
+        ]
+        , 'no-case-declarations': 'off'
+        , 'no-unsafe-finally': 'off'
+        , 'no-unreachable': 'warn'
+        , 'no-undef': 'warn'
+        , 'no-empty-pattern': 'warn'
+    }
+};
 
--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,49 +1,63 @@
 import React from 'react';
 import { useSubmit } from '@remix-run/react';
-import type { ActionFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
-import { SubmitOptions } from '@remix-run/react';
+import type { ActionFunction } from '@remix-run/node';
 import type { FormikProps } from 'formik';
 import { Formik, Form } from 'formik';
 
 
 const ContactPage = () => {
     const submit = useSubmit();
+    
+    interface TestInput {
+        input: string;
+    }
 
-    const testData = {
-        firstName: 'Alex'
-        , lastName: 'Johnson'
-        , isCool: true
+    const initialValues: TestInput = {
+        input: ''
     };
     
     const handleSubmit = ( values: any ) => {
         submit( values, { method: 'post' } ); //submit to Remix backend
     };
 
-    const initialValues: object = {
-        name: ''
-    };
     
     return (
         <Formik 
             initialValues={initialValues}
             onSubmit={( values ) => handleSubmit( values )}
         > 
-            {( { values, handleChange }: any ) => (
+            {( { values, handleChange }: FormikProps<TestInput> ) => (
                 <Form>
-                    <div>Contact</div>
-                    <input type="text" name='name' value={values.name} onChange={handleChange} />
-                    <button type='submit'>Submit</button>
+                    <h1>
+                        Test Form
+                    </h1>
+                    <input 
+                        type="text" 
+                        name='input' 
+                        value={values.input} 
+                        onChange={handleChange}
+                    />
+                    <button type='submit'>
+                        Submit
+                    </button>
                 </Form>
             )}
         </Formik>
     );
 };
 
+// Backend Remix action function
 export const action: ActionFunction = async ( { request } ) => {
-    const formData = await request.formData();
-    const data = Object.fromEntries( formData );
-    console.log( data );
+    //grab formData object off request
+    const formData = await request.formData(); 
+
+    //transform into standard object
+    const data = Object.fromEntries( formData ); 
+
+    //data is ready for usage!
+    console.log( data ); 
+
     return redirect( '/' );
 };
 
