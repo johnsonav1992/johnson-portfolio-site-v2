39f93b247a1404dc54fce17dbbf0dbd2c0fc24b2
Alex Johnson <johnsonav1992@gmail.com>
2023-01-01 13:07:47 -0600

    Set up contact test fields w/ validation

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import { useSubmit } from '@remix-run/react';
-import { redirect } from '@remix-run/node';
+import { useSubmit, useActionData } from '@remix-run/react';
+import { json, redirect } from '@remix-run/node';
 import type { ActionFunction } from '@remix-run/node';
 import type { FormikProps } from 'formik';
 import { Formik, Form } from 'formik';
+import { Button, TextField, Typography } from '@mui/material';
+import * as Yup from 'yup';
 
 
 const ContactPage = () => {
     const submit = useSubmit();
-    
-    interface TestInput {
-        input: string;
+
+    interface ContactInput {
+        name: string;
+        email: string;
+        message: string;
     }
 
-    const initialValues: TestInput = {
-        input: ''
+    const initialValues: ContactInput = {
+        name: ''
+        , email: ''
+        , message: ''
     };
+
+    const validationSchema = Yup.object( {
+        name: Yup.string().required( 'You must enter a name' )
+        , email: 
+            Yup.string()
+                .email( 'Must be a valid email address.' )
+                .required( 'You must enter an email address.' )
+        , message: Yup.string().required( 'You must enter a message.' )
+    } );
     
     const handleSubmit = ( values: any ) => {
         submit( values, { method: 'post' } ); //submit to Remix backend
@@ -24,23 +39,46 @@ const ContactPage = () => {
     
     return (
         <Formik 
-            initialValues={initialValues}
-            onSubmit={( values ) => handleSubmit( values )}
+            initialValues={ initialValues }
+            validationSchema={ validationSchema }
+            onSubmit={ ( values ) => handleSubmit( values ) }
         > 
-            {( { values, handleChange }: FormikProps<TestInput> ) => (
+            {( { values, handleChange, errors }: FormikProps<ContactInput> ) => (
                 <Form>
-                    <h1>
-                        Test Form
-                    </h1>
-                    <input 
-                        type="text" 
-                        name='input' 
-                        value={values.input} 
-                        onChange={handleChange}
+                    <Typography variant='h2'>
+                        Contact Me
+                    </Typography>
+                    <TextField 
+                        name='name'
+                        label='Name'
+                        value={ values.name }
+                        onChange={ handleChange }
+                        error={ errors.name ? true : false }
+                        helperText={ errors.name }
                     />
-                    <button type='submit'>
-                        Submit
-                    </button>
+                    <TextField 
+                        name='email'
+                        label='Email Address'
+                        value={ values.email }
+                        onChange={ handleChange }
+                        error={ errors.email ? true : false }
+                        helperText={ errors.email }
+                    />
+                    <TextField 
+                        name='message'
+                        label='Message'
+                        value={ values.message }
+                        onChange={ handleChange }
+                        error={ errors.message ? true : false }
+                        helperText={ errors.message }
+                    />
+                    <Button 
+                        color='primary'
+                        variant='contained'
+                        type='submit'
+                    >
+                        Send
+                    </Button>
                 </Form>
             )}
         </Formik>
@@ -49,16 +87,12 @@ const ContactPage = () => {
 
 // Backend Remix action function
 export const action: ActionFunction = async ( { request } ) => {
-    //grab formData object off request
     const formData = await request.formData(); 
-
-    //transform into standard object
     const data = Object.fromEntries( formData ); 
 
-    //data is ready for usage!
     console.log( data ); 
 
-    return redirect( '/' );
+    return data;
 };
 
 export default ContactPage;
