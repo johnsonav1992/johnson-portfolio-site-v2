7e753fdb262cf04e8384b01fec96b54d8f55db87
Alex Johnson <johnsonav1992@gmail.com>
2023-01-08 13:57:39 -0600

    email working but not finished

--- app/email/sendEmail.ts
+++ app/email/sendEmail.ts
@@ -12,13 +12,13 @@ export const sendEmail = async ( name: string, email: string, message: string ) 
     } );
 
     const mailOptions = {
-        from: 'ajohnson@veryableops.com'
+        from: name
         , to: `johnsonav1992@gmail.com`
-        , subject: 'Hello'
-        , text: 'Hello, World!'
+        , subject: `Message received from ${ name } - ${ email }`
+        , text: message
     };
 
-    await transporter.sendMail( mailOptions, function ( error: any, info: any ){
+    await transporter.sendMail( mailOptions, ( error: any, info: any ) => {
         if ( error ) {
             console.log( error );
         } else {
