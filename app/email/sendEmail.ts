00cae819c0e89ff2d8957c03d12608fa822c1d38
Alex Johnson <johnsonav1992@gmail.com>
2023-01-09 12:59:26 -0600

    try email templates

--- app/email/sendEmail.ts
+++ app/email/sendEmail.ts
@@ -1,5 +1,11 @@
 
-const nodemailer = require( 'nodemailer' );
+import nodemailer from 'nodemailer';
+
+// Utils
+import { formattedReceiptEmail } from './formattedReceiptEmail';
+
+// Types
+import type SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 export const sendEmail = async ( name: string, email: string, message: string ) => {
 
@@ -13,17 +19,20 @@ export const sendEmail = async ( name: string, email: string, message: string ) 
 
     const mailOptions = {
         from: name
+        , sender: name
         , to: `johnsonav1992@gmail.com`
+        , replyTo: email
         , subject: `Message received from ${ name } - ${ email }`
         , text: message
+        , html: formattedReceiptEmail()
     };
 
-    await transporter.sendMail( mailOptions, ( error: any, info: any ) => {
+    await transporter.sendMail( mailOptions, ( error: Error | null, info: SMTPTransport.SentMessageInfo ) => {
         if ( error ) {
-            console.log( error );
+            console.log( error.message );
         } else {
             console.log( 'Email sent: ' + info.response );
-        }
+        } 
     } );      
     
 };
