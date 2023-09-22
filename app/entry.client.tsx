c50d07df6cbb1c0ba3af3a5770bb1613e185bf2c
Alex Johnson <johnsonav1992@gmail.com>
2023-09-21 19:49:22 -0500

    install logrocket

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,9 @@
 import { RemixBrowser } from '@remix-run/react';
 import { StrictMode } from 'react';
 import { hydrate } from 'react-dom';
+import LogRocket from 'logrocket';
+
+LogRocket.init( '8ho0cs/portfolio-site' );
 
 // Using the old-skool React v17 hydrate() until hydrateRoot() is fixed
 hydrate ( 
